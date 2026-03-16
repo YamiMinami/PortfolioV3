9654d90709c11003bd2d8323f17388b9dc72bcf7
Sam <mathijs.sam1709@gmail.com>
2026-03-16 12:53:49 +0000

    Created branch 'Sam'

--- Portfolio/src/pages/Developers.jsx
+++ Portfolio/src/pages/Developers.jsx
@@ -1,9 +1,21 @@
 import {Link} from 'react-router-dom'
 
-const Developers [
-    {name: Trong, Age, }
+const Developers = [
+    {id: 1, name: "Trong", age: "20" },
+    {id : 2, name: "Sam", age: "26" },
 ]
 
+
 function DevelopersPage() {
-    return ()
+    return (
+        <div>
+            <h1>Meet the Developers</h1>
+                {Developers.map((dev) => (
+                    <div key={dev.id}>
+                        <h2>{dev.name}</h2>
+                        <p>{dev.age}</p>
+                    </div>))}
+        </div>
+    );
+
 }
